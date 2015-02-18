b785869271feb6a14a0b99318b72148189f33796
Zach <zbluhm@live.unc.edu>
2015-02-18 08:01:33 -0500

    Downloader test

--- src/csvDataDownloader/Main.java
+++ src/csvDataDownloader/Main.java
@@ -4,6 +4,8 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by ZachBluhm on 2/18/15.
@@ -13,25 +15,39 @@ public class Main {
     public static void main(String[]args) {
 
         String baseUrl = "http://ichart.finance.yahoo.com/table.csv?s=";
-        String ID = "GOOG";
+        String ID = "AAPL";
         String fDate = "&a=1&b=27&c=2009";
-        String tDate = "&d=0&e=15&f=2015";
+        String tDate = "&d=1&e=15&f=2015";
         String interval = "&g=d";
         final String end = "&ignore=.csv";
+        List<String> lines;
 
         try {
+            lines = FileUtils.readLines(new File("/Users/ZachBluhm/stock-tracker/Data/testloop.txt"));
 
-            URL url = new URL(baseUrl + ID + fDate + tDate + interval + end);
-            //URL url = new URL("http://finance.yahoo.com/d/quotes.csv?s=AAPL+GOOG+MSFT&f=nab");
+            for (String line : lines) {
+                ID = line;
+                try {
+                    URL url = new URL(baseUrl + ID + tDate + fDate + interval + end);
 
-            File file = new File("/Users/ZachBluhm/Desktop/stock-tracker/Data/" + ID + ".csv");
+                    File file = new File("/Users/ZachBluhm/stock-tracker/Data/CSVs/" + ID + ".csv");
 
-            FileUtils.copyURLToFile(url, file);
+                    FileUtils.copyURLToFile(url, file);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println(baseUrl+ID+fDate+tDate+interval+end);
+                }
+            }
         }
         catch (Exception e) {
             e.printStackTrace();
-            System.out.println(baseUrl+ID+fDate+tDate+interval+end);
         }
+
+
+
+
+
         //http://ichart.finance.yahoo.com/table.csv?s=GOOG&a=0&b=1&c=2013&d=3&e=1&f=2014&g=d&ignore=.csv
         //http://ichart.finance.yahoo.com/table.csv?s=APPL&a=1&b=6&c=2009&d=0&e=15&f=2015&g=d&ignore=.csv
 
