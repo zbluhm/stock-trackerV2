bc54629684d6162b01e8e7b93a77801e19ad17a6
calvinsb1023 <barkerc1@barkerc1-pc.local>
2015-02-20 16:16:43 -0500

    beta method skeletons

--- src/Core/Processor/Indicators/Beta.java
+++ src/Core/Processor/Indicators/Beta.java
@@ -5,8 +5,50 @@ package Core.Processor.Indicators;
  */
 public class Beta implements Indicator {
 
-    // TODO: find indicators to return
-    public float returnIndication(){
+    private double stockReturn;
+    private float beta;
+
+    public Beta() {
+    }
+
+    // TODO: find indicators to returns
+    public float returnIndication () {
+        setBeta();
+        return beta;
+    }
+
+    public double getStockReturn() {
         return 0;
     }
+
+    public double getStockAvgReturn() {
+        return 0;
+    }
+
+    public double getReturnMinusAvgReturn() {
+        return 0;
+    }
+
+    public double getCovar() {
+        double sum;
+
+        return 0;
+    }
+
+    public double getVar() {
+        double sum;
+
+        return 0;
+    }
+
+    public void setBeta() {
+        beta = (float) (getCovar() / getVar());
+    }
 }
+//        Sw = stock return % from previous weekly
+//        rw = market return % from previous weekly
+//        n  = number of week
+//
+//        cov(Sw, rw) = Sum((Sw@n - SwBAR)(rw@n - rwBAR)) / (n - 1)
+//        var(rw) = Sum((rw@n - rwBAR)^2) / (n - 1)
+//        b = cov(Sw, rw) / var(rw)
