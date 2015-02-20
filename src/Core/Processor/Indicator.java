7bc70de021bbec95cbab19af2b108dc9c4076f7e
Zach <zbluhm@live.unc.edu>
2015-02-20 14:31:53 -0500

    Added some more TODOs

--- src/Core/Processor/Indicator.java
+++ src/Core/Processor/Indicator.java
@@ -5,6 +5,8 @@ package Core.Processor;
  */
 public interface Indicator {
 
+    public float returnIndication(); //An Indicator object must be able to return some value that relays the trend of the stock
+                                     //(SUBJECT TO CHANGE) This can be on a scale of x>1 || x<1. If x = 1 then the trend is sideways.
 
 
 
