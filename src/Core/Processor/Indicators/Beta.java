ed616575c1e6ae8a975f30d2685c84cb514838c4
calvinsb1023 <barkerc1@barkerc1-pc.local>
2015-02-20 15:28:52 -0500

    beta class
    beta started

---
+++ src/Core/Processor/Indicators/Beta.java
@@ -0,0 +1,12 @@
+package Core.Processor.Indicators;
+
+/**
+ * Created by barkerc1 on 2/20/15.
+ */
+public class Beta implements Indicator {
+
+    // TODO: find indicators to return
+    public float returnIndication(){
+        return 0;
+    }
+}
