163a6d16acedc0354db8a7001675b4f603b538bf
Zach <zbluhm@live.unc.edu>
2015-02-22 17:52:53 -0500

    Started BollingerBands Class

---
+++ src/Core/Processor/Indicators/BollingerBands.java
@@ -0,0 +1,24 @@
+package Core.Processor.Indicators;
+
+/**
+ * Created by ZachBluhm on 2/22/15.
+ */
+public class BollingerBands extends IndicatorClass implements Indicator {
+
+    public BollingerBands(String stock) {
+        super(stock);
+    }
+
+    public void getBollingerBands() {
+
+    }
+
+
+
+
+
+    @Override
+    public float returnIndication() {
+        return 0;
+    }
+}
